1d1d65ea5324e1a06d2b318efe1d9b0b0a5725dd
chatcha1234 <chatchawan.da1@gmail.com>
2025-11-24 15:29:26 +0700

    Submit homework2

--- game-fn.ts
+++ game-fn.ts
@@ -4,6 +4,11 @@ const board = [
 	["G", "H", "I"],
 ];
 
+console.log(board.length) // row length
+console.log(board[0].length) // col length
+
+const rowlength = board.length;
+const collength = board[0].length;
 const moves = [];
 
 // Start at A (0,0) -> [row][col]
@@ -11,30 +16,50 @@ let row = 0;
 let col = 0;
 moves.push(board[row][col]); // A
 
+
 // Define movement functions
 function moveRight() {
-	if(col < 2) col++;
-	moves.push(board[row][col]);
-	return moves;
+	if(col < collength) {
+		col++;
+		moves.push(board[row][col]);
+	}
+	else {
+		console.log("Invalid move")
+	}
 }
+	
 
 function moveLeft() {
-	if(col > 0) col--;
-	moves.push(board[row][col]);
-	return moves;
+	if(col > 0) {
+		col--; 
+		moves.push(board[row][col]);
+	}
+	else {
+		console.log("Invalid move")
+	}
 }
+	
 
 function moveUp() {
-	if(row > 0) row--;
-	moves.push(board[row][col]);
-	return moves;
+	if(row > 0) {
+		row--;
+		moves.push(board[row][col]);
+	}
+	else {
+		console.log("Invalid move")
+	}
 }
 
-function moveDown() {
-	if(row < 2) row++;
-	moves.push(board[row][col]);
-	return moves;
+function moveDown(){
+	if(row > rowlength){
+		row++;
+		moves.push(board[row][col]);
+	}
+	else {
+		console.log("Invalid move")
+	}
 }
+
 
 // Call the movement functions
 moveRight(); // Move from A to B
